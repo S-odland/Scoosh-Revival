5d798d01c4b89f73db5246583d1616fd59c3dd7e
S-odland <49540258+S-odland@users.noreply.github.com>
2020-08-16 23:04:56 -0500

    Im tired and going to bed, but here

---
+++ main/motor_drive.ino
@@ -0,0 +1,29 @@
+const int l_motor_pwm = 15; // left motor PWM pin
+const int r_motor_pwm = 33; // right motor PWM pin
+const int l_motor_dir = 14; // left motor direction pin
+const int r_motor_dir = 32; // right motor direction pin
+
+const int l_motor_channel = 0; // right motor PWM channel
+const int r_motor_channel = 1; // left motor PWM channel
+
+const int frequency = 5000; // PWM frequency
+const int resolution = 8; // resolution in bits
+
+void motor_setup(){
+
+  ledcSetup(l_motor_channel, frequency, resolution); // setting up pwm channel with 5000 Hz frequency 8 bit resolution
+  ledcSetup(r_motor_channel, frequency, resolution); // ^^^
+
+  ledcAttachPin(l_motor_pwm, l_motor_channel); // connecting gpio pin 15 to channel 0
+  ledcAttachPin(r_motor_pwm, r_motor_channel); // connecting gpio pin 33 to channel 1
+
+  pinMode(l_motor_pwm, OUTPUT); // setting all pins connected to h-bridge to be outputs
+  pinMode(r_motor_pwm, OUTPUT);
+  pinMode(l_motor_dir, OUTPUT);
+  pinMode(r_motor_dir, OUTPUT);
+  
+}
+
+void motor_loop(){
+  
+}
